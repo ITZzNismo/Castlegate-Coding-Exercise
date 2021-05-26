db89823a516bef873e05a90e2bb5769ceb3f6d22
ITZzNismo <simonpowell2324@gmail.com>
2021-05-26 10:36:00 +0100

    Added mobile layout update, added mobile navbar functionality

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function openMobileNav() {
+    document.getElementById("mobile-nav-menu").style.width = "10%";
+}
+
+function closeMobileNav() {
+    document.getElementById("mobile-nav-menu").style.width = "0%";
+}
